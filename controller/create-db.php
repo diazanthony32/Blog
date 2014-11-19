a1f59d7926e324506799e931bb55bd1286c0bebc
Anthony Diaz <diazanthony32@gmail.com>
2014-11-19 14:52:44 -0800

    Videos 7 & 8
    Made variables to be able to make a connection to the server

---
+++ controller/create-db.php
@@ -0,0 +1,7 @@
+<?php
+
+//include the info in the file
+require_once("../model/database.php");
+
+//enable us to access info on the server
+$connection = new mysqli($host, $user, $password);
---
+++ model/database.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$host = "localhost";
+
+//username of server
+$username = "root";
+
+//password of server
+$password = "root";
+
+//database name
+$database = "blog_db";
