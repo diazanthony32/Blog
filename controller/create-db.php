4e54b741eee68e271862502f65e50d9187f50f2a
Anthony Diaz <diazanthony32@gmail.com>
2014-12-01 14:44:50 -0800

    Fixed the file directory on the index and createdb files

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,7 +1,25 @@
 <?php
 
 //include the info in the file
-require_once("../model/database.php");
+require_once(__DIR__ ."/../model/database.php");
 
 //enable us to access info on the server
 $connection = new mysqli($host, $user, $password);
+
+//checks if the connection is unsuccesful or not
+if($connection->connect_error){
+    
+    //Connection has failed and shos the problem
+    die("Error: " . $connection->connect_error);
+    
+}
+
+ else {
+     
+    //Connection Successful
+    echo 'Success: ' . $connection->host_info;
+    
+ }
+ 
+ //ends the connectiion to the server
+ $connection->close();
