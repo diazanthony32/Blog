e231aa90bc82444c26317065161895e7a2dc10eb
Anthony Diaz <diazanthony32@gmail.com>
2014-12-01 14:33:53 -0800

    Checks if the connection to the server is successful or not

--- model/database.php
+++ model/database.php
@@ -11,3 +11,21 @@ $password = "root";
 
 //database name
 $database = "blog_db";
+
+//checks if the connection is unsuccesful or not
+if($connection->connect_error){
+    
+    //Connection has failed and shos the problem
+    die("Error: " . $connection->connect_error);
+    
+}
+
+ else {
+     
+    //Connection Successful
+    echo 'Success' . $connection->host_info;
+    
+ }
+ 
+ //ends the connectiion to the server
+ $connection->close();
