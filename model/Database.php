7b278476d5ef174d2c2cd99fc7516b3e348fd4fb
Anthony Diaz <diazanthony32@gmail.com>
2015-02-06 22:04:42 -0800

    Implementing the openConnection function

--- model/Database.php
+++ model/Database.php
@@ -1,35 +1,39 @@
 <?php
 
-class Database{
-    
+class Database {
+
     private $connection;
     private $host;
     private $username;
     private $password;
     private $database;
-    
+
     public function __construct($host, $username, $password, $database) {
-        
+
         $this->host = $host;
-        $this->password =$password;
+        $this->password = $password;
         $this->username = $username;
         $this->database = $database;
-    
     }
-    
-    public function openConnection(){
-        
-        
-    }
-    
-    public function closeConnection(){
-        
+
+    public function openConnection() {
+
+        $this->connection = new mysqli($this->host, $this->user, $this->password, $this->database);
+
+        //checks if the connection is unsuccesful or not
+        if ($this->connection->connect_error) {
+
+            //Connection has failed and shos the problem
+            die("<p>Error: " . $this->connection->connect_error) . "</p>";
+        }
         
     }
 
-    public function query(){
+    public function closeConnection() {
         
-        
+    }
+
+    public function query($string) {
         
     }
 
