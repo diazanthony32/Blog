2efc921727b90e9dc3d8fbbc87d50dad6ac7c2bb
Anthony Diaz <diazanthony32@gmail.com>
2015-02-06 23:01:52 -0800

    PHP - Blog - 36 Updating code to use session variables

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,7 +4,7 @@
 require_once(__DIR__ . "/../model/config.php");
 
 //created a query that will create a table in order to put information
-$query = $connection->query("CREATE TABLE posts ("
+$query = $_SESSION["connection"]->query("CREATE TABLE posts ("
         . "id int(11) NOT NULL AUTO_INCREMENT,"
         . "title varchar(255) NOT NULL,"
         . "post text NOT NULL,"
@@ -20,7 +20,7 @@ if($query){
 //checks if the table was already there or created improperly
  else {
      
-    echo "<p>$connection->error</p>";
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
      
 }
 
--- controller/create-post.php
+++ controller/create-post.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . "/../model/config.php");
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
-$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+$query = $_SESSION["connection"]->query("INSERT INTO posts SET title = '$title', post = '$post'");
 
 if ($query){
     
@@ -14,6 +14,6 @@ if ($query){
 }
  else {
 
-    echo "<p>$connection->error</p>";
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
      
 }
